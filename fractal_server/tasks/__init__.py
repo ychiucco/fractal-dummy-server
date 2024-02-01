fa5c6c622686ec0b461376aa1dfead82076cab3c
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-02-01 16:08:03 +0100

    restore tasks folder

---
+++ fractal_server/tasks/__init__.py
@@ -0,0 +1,8 @@
+"""
+`tasks` module
+
+Submodules:
+    * [Endpoint functions](background_operations)
+    * [Background-task functions](endpoint_operations)
+    * [Auxiliary functions](utils)
+"""
